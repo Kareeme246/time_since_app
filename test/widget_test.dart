c9ee122206149ace81b0b550a28707ca7e7518cf
KareemE246 <kareemelgohry1@gmail.com>
2024-10-15 16:44:51 -0500

    Add packages

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:time_since_app_deploy/main.dart';
+import 'package:time_since_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
